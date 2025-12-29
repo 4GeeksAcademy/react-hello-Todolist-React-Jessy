54d4475028dc90b170a3b603b276aae6990c762b
Jessy-Espinoza <jessy_2388@yahoo.es>
2025-12-29 19:53:26 +0000

    ToDoList Jessy

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,47 +1,99 @@
-import { useState } from "react"
+import { useState } from "react";
+import "../components/ToDoList"
 
 export const ToDoList = () => {
-    //const listaDeTareas=["arreglar", "cocinar", "descansar"]
-    const [listaDeTareas, setListaDeTareas] = useState([]);
-    const [nuevaTarea, setNuevaTarea] = useState([""]);
+  const [listaDeTareas, setListaDeTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState("");
 
-    const agregarTarea = () => {
-        if (nuevaTarea.trim() === "") { return }
-        setListaDeTareas((hola) => {
-            console.log(hola);
-            return [...hola, nuevaTarea]
-        })
-        setNuevaTarea("");
-    };
+  const agregarTarea = () => {
+    if (nuevaTarea.trim() === "") return;
 
-    const eliminarTarea = () => {
-        setListaDeTareas(listaDeTareas.filter((index) => index)
-    }
+    setListaDeTareas(prev => [...prev, nuevaTarea]);
+    setNuevaTarea("");
+  };
 
-    return (
-        <div className="text-center d-flex flex-column justify-content-center align-items-center m-1">
-            hola soy todolist
-            <input className="btn btn-outline-secondary"
-                type="text"
-                value={nuevaTarea}
-                onChange={(e) => setNuevaTarea(e.target.value)}
-                placeholder="Escribe una tarea"
-            />
-            <button className="btn btn-info" onClick={agregarTarea} >
-                agregar tarea
+  const eliminarTarea = (indiceclickado) => {
+    console.log("indiceclickado es ", indiceclickado)
+    setListaDeTareas(prev =>
+      prev.filter((_, indice) => indice !== indiceclickado)
+    );
+  };
+
+  const cantidadTareas = listaDeTareas.length;
+  let mensaje = "";
+
+  if (cantidadTareas === 0) {
+    mensaje = "No tienes tareas pendientes 🎉";
+  } else if (cantidadTareas >= 1 && cantidadTareas <= 4) {
+    mensaje = `Tienes ${cantidadTareas} tareas pendientes 😅​`;
+  } else if (cantidadTareas >= 5 && cantidadTareas <= 8) {
+    mensaje = `¡Tienes ${cantidadTareas} tareas pendientes!  😲​​`;
+  }
+  else if (cantidadTareas >= 8) {
+    mensaje = `¡Tienes ${cantidadTareas} tareas pendientes!  😭​​​`;
+  }
+
+  return (
+
+    <div className="app-container d-flex flex-column align-items-center mt-5">
+      <h3 className="mb-4">📝 To Do List</h3>
+
+      <div className="d-flex mb-2">
+        <input
+          className="form-control form-control-sm me-2"
+          style={{ width: "220px" }}
+
+          type="text"
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && agregarTarea()}
+
+          placeholder="Escribe una tarea"
+        />
+        <button className="btn btn-info mt-2" onClick={agregarTarea}>
+          Agregar tarea
+        </button>
+      </div>
+
+      <ul className="list-group" style={{ width: "350px" }}>
+        {listaDeTareas.map((tarea, indice) => (
+          <li
+            key={indice}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            {tarea}
+
+            <button data-bs-toggle="modal" data-bs-target={`#modal${indice}`}
+              type="button"
+            >
+              ❌
             </button>
-            <ul>
 
-                {
-                    listaDeTareas.map((tarea, indice) => {
-                        return <li key={indice}>{tarea}
-                            <button type="button" className="btn-close" onClick={eliminarTarea} disabled aria-label="Close"></button> </li>
-                    })
-                }
-            </ul>
+            <div className="modal fade" id={`modal${indice}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title center fs-5" id="exampleModalLabel">Estas a punto de eliminar la tarea</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                    Estas seguro que deseas continuar ?
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => eliminarTarea(indice)}>Aceptar</button>
+                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Cancelar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <p className={`mt-2 tareas-texto`}>
+        {mensaje}
+      </p>
+    </div>
+  );
+};
 
 
-        </div>
-
-    )
-}
